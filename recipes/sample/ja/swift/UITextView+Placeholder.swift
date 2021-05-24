b13115f05868c38be091d445208cb89012b6d41a
uruly <rk.uru007@gmail.com>
2021-05-24 13:39:29 +0900

    Add swift file to sample

---
+++ recipes/sample/ja/swift/UITextView+Placeholder.swift
@@ -0,0 +1,80 @@
+//
+//  UITextView+Placeholder.swift
+//  TODOReport
+//
+//  Created by Reona Kubo on 2020/08/30.
+//  Copyright © 2020 Reona Kubo. All rights reserved.
+//
+
+import UIKit
+
+extension UITextView: UITextViewDelegate {
+
+    /// Resize the placeholder when the UITextView bounds change
+    override open var bounds: CGRect {
+        didSet {
+            self.resizePlaceholder()
+        }
+    }
+
+    /// The UITextView placeholder text
+    public var placeholder: String? {
+        get {
+            var placeholderText: String?
+
+            if let placeholderLabel = self.viewWithTag(100) as? UILabel {
+                placeholderText = placeholderLabel.text
+            }
+
+            return placeholderText
+        }
+        set {
+            if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
+                placeholderLabel.text = newValue
+                placeholderLabel.sizeToFit()
+            } else {
+                self.addPlaceholder(newValue!)
+            }
+        }
+    }
+
+    /// When the UITextView did change, show or hide the label based on if the UITextView is empty or not
+    ///
+    /// - Parameter textView: The UITextView that got updated
+    public func updatePlaceholder() {
+        if let placeholderLabel = self.viewWithTag(100) as? UILabel {
+            placeholderLabel.isHidden = self.text.count > 0
+        }
+    }
+
+    /// Resize the placeholder UILabel to make sure it's in the same position as the UITextView text
+    func resizePlaceholder() {
+        if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
+            let labelX = self.textContainer.lineFragmentPadding
+            let labelY = self.textContainerInset.top
+            let labelWidth = self.frame.width - (labelX * 2)
+            let labelHeight = placeholderLabel.frame.height
+
+            placeholderLabel.frame = CGRect(x: labelX, y: labelY, width: labelWidth, height: labelHeight)
+        }
+    }
+
+    /// Adds a placeholder UILabel to this UITextView
+    private func addPlaceholder(_ placeholderText: String) {
+        let placeholderLabel = UILabel()
+
+        placeholderLabel.text = placeholderText
+        placeholderLabel.sizeToFit()
+
+        placeholderLabel.font = self.font
+        placeholderLabel.textColor = .systemGray
+        placeholderLabel.tag = 100
+
+        placeholderLabel.isHidden = self.text.count > 0
+
+        self.addSubview(placeholderLabel)
+        self.resizePlaceholder()
+        //        self.delegate = self
+    }
+
+}
